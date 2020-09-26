3ec82be3be5f3b298e35c8c1f730d984ae410144
hackerman <3372410+aeneasr@users.noreply.github.com>
2020-09-26 10:36:00 +0200

    fix: improve scripts

--- create.sh
+++ create.sh
@@ -4,29 +4,23 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew install git golang node kubernetes-cli mysql-client git-town \
   skaffold helm nancy circleci cockroach composer gettext terraform \
-  gh gnupg gnutls wget sqlite postgresql golangci-lint gometalinter goreleaser hashcat heroku imagemagick
-brew cask install gpg-suite jdownloader minikube ngrok pgadmin4 \
+  gh gnupg gnutls wget sqlite postgresql golangci-lint gometalinter goreleaser hashcat heroku imagemagick minikube
+brew cask install gpg-suite jdownloader ngrok pgadmin4 \
   veracrypt virtualbox wireshark db-browser-for-sqlite google-cloud-sdk
 
 # MySQL
-echo 'export PATH="/usr/local/opt/mysql-client/bin:$PATH"' > ~/.zshenv
-echo 'export PATH="/usr/local/opt/mysql-client/bin:$PATH"' > ~/.bash_profile
+echo 'export PATH="/usr/local/opt/mysql-client/bin:$PATH"' >> ~/.zshenv
+echo 'export PATH="/usr/local/opt/mysql-client/bin:$PATH"' >> ~/.bash_profile
 
 ## Make mysqldump compatible with versions prior to 8.0
-echo '[mysqldump]' > ~/.my.cnf 
-echo 'column-statistics=0' > ~/.my.cnf 
+echo '[mysqldump]' >> ~/.my.cnf 
+echo 'column-statistics=0' >> ~/.my.cnf 
 
 source ~/.zshenv
 
-# Git
-git config --global user.name "aeneasr"
-git config --global user.name "3372410+aeneasr@users.noreply.github.com"
-
 # Global gitignore
-git config --global core.excludesfile ~/.gitignore
-git config --global core.excludesfile ~/.gitignore
-echo ".idea/" ~/.gitignore
-echo ".DS_Store" ~/.gitignore
+echo ".idea/" >> ~/.gitignore
+echo ".DS_Store" >> ~/.gitignore
 
 # Go
 mkdir -p ~/workspace/go
